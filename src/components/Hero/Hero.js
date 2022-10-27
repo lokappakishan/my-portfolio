ed9585bcf2f46c5491d5dd6118296d6caa9c7cbe
lokappakishan <lokappakishan@gmail.com>
2022-10-27 15:27:27 +0200

    adding hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,16 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle>Welcome To <br />
+       my personal po</SectionTitle>
+      <SectionText>
+        Frontend developer with more than three years' experience in building beautiful and functional responsive user interfaces using HTML, CSS and JavaScript frameworks.
+      </SectionText>
+      <Button onClick={()=>{window.location = 'https://lokappakishan@gmail.com'}}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
